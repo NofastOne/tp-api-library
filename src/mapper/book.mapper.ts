ffeeab790ea4c7856ef1187018b9e1a9cb9510d0
NofastOne <ninomegard1@gmail.com>
2025-09-26 15:27:00 +0200

    correctif route GET /books/{id} -> passage par dataValues

--- src/mapper/book.mapper.ts
+++ src/mapper/book.mapper.ts
@@ -2,5 +2,5 @@ import { BookDTO } from "../dto/book.dto";
 import { Book } from "../models/book.model";
 
 export function toDto(book: Book): BookDTO {
-    return {id: book.id, title: book.title, publishYear: book.publishYear, author: book.author, isbn: book.isbn  };
+    return {id: book.dataValues.id, title: book.dataValues.title, publishYear: book.dataValues.publishYear, author: book.dataValues.author, isbn: book.dataValues.isbn  };
 }
